4b00e6861b54ec2a13eb7a8c7d887e5347c49555
KseniyaUA <59832590+KseniyaUA@users.noreply.github.com>
2020-03-16 18:22:48 +0200

    ДипломКсения

---
+++ script/js1.js
@@ -0,0 +1,16 @@
+<!--
+        function Complete()
+        {
+            var Elem="Фамилия: " + document.Sel1.Family.value +
+             	"\nЯкаТваринка: " + document.Sel1.AnimalClass.value +
+                "\nИмя: " + document.Sel1.Name.value +
+                "\nСтать: " + document.Sel1.Sex.value +
+                "\nВозраст: " + document.Sel1.Age.value +
+                "\nТелефон: " + document.Sel1.Phone.value;
+            alert(Elem);
+        }
+       
+function check()
+{ 
+  document.getElementById('t1').value= ...;
+}
